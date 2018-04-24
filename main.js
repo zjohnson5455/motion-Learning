a3a5080d66c7bee6acf7acb9dffc35d12cd7be0a
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-04-24 10:34:32 -0400

    can now draw. toolbar not functional yet

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-let radius = 10;
+let radius = 4;
+var dragging = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+context.lineWidth = radius * 2;
+
 var putPoint = function(e) {
-  context.beginPath();
-  context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
-  context.fill();
+  if (dragging) {
+    context.lineTo(e.clientX, e.clientY);
+    context.stroke();
+    context.beginPath();
+    context.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
+    context.fill();
+    context.beginPath();
+    context.moveTo(e.clientX, e.clientY);
+  }
 }
 
-canvas.addEventListener('mousedown', putPoint);
+var engage = function(e) {
+  dragging = true;
+  putPoint(e);
+}
+
+var disengage = function() {
+  dragging = false;
+  context.beginPath();
+}
+
+canvas.addEventListener('mousedown', engage);
+canvas.addEventListener('mousemove', putPoint);
+canvas.addEventListener('mouseup', disengage);
